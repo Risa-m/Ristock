e7572c91723b6ddb06b71c188b9749ab9d7bd345
Risa <rinrinrs.510@gmail.com>
2020-04-27 11:32:01 +0900

    firebase config & routing

--- src/App.js
+++ src/App.js
@@ -6,10 +6,45 @@ import './App.css';
 //const Home = lazy(() => import('./Home'));
 import { Home } from './view/Home.js'
 import { Stocks } from './view/Stocks.js'
+import { StockList } from './view/StockList.js'
+
+import firebase from './firebase';
+import Button from '@material-ui/core/Button';
+
+import Auth from './components/Auth.js'
+import { AddStock } from './view/AddStock';
+
+class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      isSigned: false,
+      user: null
+    }
+
+    this.isSigned = this.isSigned.bind(this)
+    this.setUser = this.setUser.bind(this)
+  }
+
+  isSigned(isSigned) {
+    console.log("callback isSigned", isSigned)
+    this.setState({isSigned : isSigned})
+  }
+
+  setUser(user){
+    console.log("callback user", user)
+    this.setState({user : user})
+  }
+
+  logout(){
+    firebase.auth().signOut()
+    this.setState({isSigned: false, user: null})
+  }
 
 
-function App() {
-  return (
+  render(){
+    return (
     <BrowserRouter>
     <div className="App-root">
       <div className="switch-view">
@@ -17,25 +52,26 @@ function App() {
       <li><Link to='/'>Home</Link></li>
       <li><Link to='/stocks/add'>Add Stocks</Link></li>
       <li><Link to='/stocks'>List</Link></li>
+      {//(this.state.isSigned)? <li><Button onClick={this.logout}>logout</Button></li> : null
+      }
       </ul>
 
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/stocks' component={Stocks} />
-        <Route exact path='/stocks/:id' render={props => <Stocks match={props.match} />} />
-
-        {//<Route exact path='/stocks/add' component={AddStock} />
-        //<Route exact path='/stocks/list' component={StockList} />
-        }
-
-        {//<Route exact path='/works/:id' render={props => <WorksItem match={props.match} />} />
-        }
-
+        <Route exact path='/' component={Home}/>
+    
+        <Auth setUser={this.setUser} isSigned={this.isSigned}>
+          <Switch>
+            <Route exact path='/stocks' render={props => <StockList user={this.state.user} />} />
+            <Route exact path='/stocks/add' render={props => <AddStock /*match={props.match}*/ user={this.state.user}/>} />
+            <Route render={() => <p>not found.</p>}/>
+          </Switch>
+        </Auth>
       </Switch>
       </div>
     </div>
     </BrowserRouter>
-  );
+    );
+  }
 }
 
 export default App;
