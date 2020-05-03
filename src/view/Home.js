e48c2c822908d4cbdd331ca79c85614066dae8e2
Risa <rinrinrs.510@gmail.com>
2020-05-03 16:26:05 +0900

    ログイン後にメニュー表示

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,14 +1,45 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import firebase, { db } from '../firebase';
+import Button from '@material-ui/core/Button';
 
-export const Home = () => {
+export class Home extends React.Component {
 
+  constructor(props){
+    super(props)
+
+    this.state = {
+      user: this.props.user
+    }
+  }
   
-  return (
-  <div className="home-root">
-      Home View
-  </div>)
+  render(){
+
+    if(this.props.user){
+      return (
+        <div>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/stocks/add'>Add Stocks</Link></li>
+            <li><Link to='/stocks'>List</Link></li>
+            {(this.state.isSigned)? <li><Button onClick={this.logout}>logout</Button></li> : null
+            }
+          </ul>
+
+          <p>logged in at Home</p>
+          <Link to="stocks">stocks list</Link>
+        </div>
+
+      )
+    }else{
+
+    return (
+      <div className="home-root">
+          please <Link to={"/login"}>login</Link>...
+      </div>
+      )
+    }
+  }
 }
 
 
