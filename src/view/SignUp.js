7989f1e1b4b771ff3e75412256ffbea4d8ca51d3
Risa <rinrinrs.510@gmail.com>
2021-01-03 00:17:14 +0900

    sign upページのデザインを修正

--- src/view/SignUp.js
+++ src/view/SignUp.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import 'asset/views.css';
 
+import Paper from '@material-ui/core/Paper';
+
 export class SignUp extends React.Component{ 
   constructor(){
     super()
@@ -11,10 +13,17 @@ export class SignUp extends React.Component{
 
     return(
         <div className="signup-root">
-          <p>ご利用にはGoogleアカウントでのログインが必要です。</p>
-          <Link to="login" title="Sign in with Google Account">
-            <img src="image/google_signin_normal.png" width="180" alt="Sign in with Google"/>
-          </Link>
+          <h2>ログイン・新規登録</h2>
+          <Paper variant="outlined" className="signup-outline">
+
+          <p>お持ちのアカウントで登録/ログイン</p>
+          <div className="signup-logo-image">
+            <Link to="login" title="Sign in with Google Account">
+              <img src="image/google_signin_normal.png" width="180" alt="Sign in with Google"/>
+            </Link>
+          </div>
+
+          </Paper>
         </div>
     )
   }
